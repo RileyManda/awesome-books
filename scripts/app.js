bcbd8a408721f289e9b1d1a321080078d65739c6
RileyManda <rileymanda69@gmail.com>
2023-06-05 14:23:43 +0200

    Implemented Remove book

--- scripts/app.js
+++ scripts/app.js
@@ -2,18 +2,21 @@
 const booksCollection = [
   {
     title: 'JavaScript The Good Parts',
-    author: 'David Flanagan ',
+    author: 'David Flanagan',
   },
   {
     title: 'Beginning JavaScript',
-    author: 'Paul Wilton et all ',
+    author: 'Paul Wilton et all',
   },
   {
     title: 'JavaScript and jQuery',
     author: 'Jon Duckett',
   },
+  {
+    title: 'Remove Author Test',
+    author: 'Remove Test',
+  },
 ];
-
 
 // Get the books container
 const bookContainer = document.querySelector('.books-container');
@@ -23,21 +26,29 @@ const bookElements = booksCollection.map((book) => {
   const bookElement = document.createElement('div');
   bookElement.classList.add('book');
 
-  const titleElement = document.createElement("p");
-  titleElement.classList.add("book-title");
+  const titleElement = document.createElement('p');
+  titleElement.classList.add('book-title');
   titleElement.textContent = book.title;
 
-  const authorElement = document.createElement("p");
-  authorElement.classList.add("book-author");
+  const authorElement = document.createElement('p');
+  authorElement.classList.add('book-author');
   authorElement.textContent = book.author;
-  
-  const removeButton = document.createElement("button");
-  removeButton.classList.add("btn", "remove");
-  removeButton.textContent = "Remove";
 
-  bookContainer.appendChild(titleElement);
-  bookContainer.appendChild(authorElement);
-  bookContainer.appendChild(removeButton);
+  const removeButton = document.createElement('button');
+  removeButton.classList.add('btn', 'remove');
+  removeButton.textContent = 'Remove';
+
+  removeButton.addEventListener('click', () => {
+    // Traverse up the DOM to find the parent book element
+    const parentBookElement = removeButton.closest('.book');
+    if (parentBookElement) {
+      bookContainer.removeChild(parentBookElement);
+    }
+  });
+
+  bookElement.appendChild(titleElement);
+  bookElement.appendChild(authorElement);
+  bookElement.appendChild(removeButton);
 
   return bookElement;
 });
