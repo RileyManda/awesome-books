46a3fc7985d9e0aa352f3e7f3f16359f85ed8df3
lily4178993 <nellytelli@gmail.com>
2023-06-05 11:53:27 +0000

    Added a display function

--- scripts/app.js
+++ scripts/app.js
@@ -13,4 +13,35 @@ const booksCollection = [
     author: 'Jon Duckett',
   },
 ];
-console.log(booksCollection);
+
+
+// Get the books container
+const bookContainer = document.querySelector('.books-container');
+
+// Function to display book
+const bookElements = booksCollection.map((book) => {
+  const bookElement = document.createElement('div');
+  bookElement.classList.add('book');
+
+  const titleElement = document.createElement("p");
+  titleElement.classList.add("book-title");
+  titleElement.textContent = book.title;
+
+  const authorElement = document.createElement("p");
+  authorElement.classList.add("book-author");
+  authorElement.textContent = book.author;
+  
+  const removeButton = document.createElement("button");
+  removeButton.classList.add("btn", "remove");
+  removeButton.textContent = "Remove";
+
+  bookContainer.appendChild(titleElement);
+  bookContainer.appendChild(authorElement);
+  bookContainer.appendChild(removeButton);
+
+  return bookElement;
+});
+
+bookElements.forEach((bookElement) => {
+  bookContainer.appendChild(bookElement);
+});
